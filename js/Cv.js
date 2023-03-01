55d16927245de50b70f8ddc0f44333ddccdf88ce
MarcosJMendoza <126634878+MarcosJMendoza@users.noreply.github.com>
2023-03-01 00:09:56 -0300

    First commit

---
+++ js/Cv.js
@@ -0,0 +1,7 @@
+document.getElementById('facebook').addEventListener('click', function(){
+    location.href ='https://www.facebook.com/';
+});
+
+document.getElementById('instagram').addEventListener('click', function(){
+    location.href ='https://www.instagram.com/';
+});
